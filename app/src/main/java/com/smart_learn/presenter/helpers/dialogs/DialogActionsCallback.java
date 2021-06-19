0bc54614a1b042c1899571eeaea031ba55b96796
Marius-RO <blw_matt@rocketmail.com>
2021-06-19 11:57:29 +0300

    Remove unnecessary files
    * Remove files replaced in commit c0855bb . Check that commit message for
      more info.

--- app/src/main/java/com/smart_learn/presenter/helpers/dialogs/DialogActionsCallback.java
+++
@@ -1,8 +0,0 @@
-package com.smart_learn.presenter.helpers.dialogs;
-
-import android.content.DialogInterface;
-
-public interface DialogActionsCallback <T> {
-    void onShowDialog(DialogInterface dialogInterface, T dialogBinding);
-    default void onPositiveButtonPressed(DialogInterface dialog, T dialogBinding){}
-}
--- app/src/main/java/com/smart_learn/presenter/helpers/dialogs/DialogDismissCallback.java
+++
@@ -1,5 +0,0 @@
-package com.smart_learn.presenter.helpers.dialogs;
-
-public interface DialogDismissCallback {
-    void onDismiss();
-}
