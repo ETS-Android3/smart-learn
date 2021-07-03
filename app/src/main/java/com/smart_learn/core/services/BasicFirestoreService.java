e9e5453b001b2b4090dfbf116b8edf6bb40451b5
Marius-RO <blw_matt@rocketmail.com>
2021-07-03 12:30:37 +0300

    Add basic Firestore repository and service
    * Add BasicFirestoreRepository and BasicFirestoreService for handling operations with Firebase
      Firestore.

---
+++ app/src/main/java/com/smart_learn/core/services/BasicFirestoreService.java
@@ -0,0 +1,39 @@
+package com.smart_learn.core.services;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.smart_learn.data.firebase.firestore.repository.BasicFirestoreRepository;
+import com.smart_learn.data.helpers.DataCallbacks;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+
+public abstract class BasicFirestoreService <T, R extends BasicFirestoreRepository<T>> {
+
+    protected final R repository;
+
+    public BasicFirestoreService(@NonNull @NotNull R repository) {
+        this.repository = repository;
+    }
+
+    public void addDocument(@NonNull @NotNull T item, @NonNull @NotNull CollectionReference collectionReference,
+                            @Nullable DataCallbacks.InsertUpdateDeleteCallback<DocumentReference> callback){
+        repository.addDocument(item, collectionReference, callback);
+    }
+
+    public void updateDocument(@NonNull @NotNull Map<String,Object> updatedInfo, @NonNull @NotNull DocumentSnapshot documentSnapshot,
+                               @Nullable DataCallbacks.InsertUpdateDeleteCallback<DocumentSnapshot> callback){
+        repository.updateDocument(updatedInfo, documentSnapshot, callback);
+    }
+
+    public void deleteDocument(@NonNull @NotNull DocumentSnapshot documentSnapshot,
+                               @Nullable DataCallbacks.InsertUpdateDeleteCallback<DocumentSnapshot> callback){
+        repository.deleteDocument(documentSnapshot, callback);
+    }
+
+}
---
+++ app/src/main/java/com/smart_learn/data/firebase/firestore/repository/BasicFirestoreRepository.java
@@ -0,0 +1,100 @@
+package com.smart_learn.data.firebase.firestore.repository;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.smart_learn.data.helpers.DataCallbacks;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+
+import timber.log.Timber;
+
+public abstract class BasicFirestoreRepository <T> {
+
+    public static final String COLLECTION_NOTIFICATIONS = "notifications";
+    public static final String COLLECTION_USERS = "users";
+
+    public void addDocument(@NonNull @NotNull T item, @NonNull @NotNull CollectionReference collectionReference,
+                            @Nullable DataCallbacks.InsertUpdateDeleteCallback<DocumentReference> callback){
+
+        collectionReference
+                .add(item)
+                .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                    @Override
+                    public void onComplete(@NonNull @NotNull Task<DocumentReference> task) {
+                        if(task.isSuccessful()){
+                            if(callback != null){
+                                callback.onSuccess(task.getResult());
+                            }
+                            return;
+                        }
+
+                        // here add operation failed
+                        Timber.w(task.getException());
+                        if(callback != null){
+                            callback.onFailure(null);
+                        }
+                    }
+                });
+    }
+
+
+    public void updateDocument(@NonNull @NotNull Map<String,Object> updatedInfo, @NonNull @NotNull DocumentSnapshot documentSnapshot,
+                               @Nullable DataCallbacks.InsertUpdateDeleteCallback<DocumentSnapshot> callback){
+
+        documentSnapshot.getReference()
+                .update(updatedInfo)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull @NotNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            if(callback != null){
+                                callback.onSuccess(documentSnapshot);
+                            }
+                            return;
+                        }
+
+                        // here update operation failed
+                        Timber.w(task.getException());
+                        if(callback != null){
+                            callback.onFailure(documentSnapshot);
+                        }
+                    }
+                });
+    }
+
+
+    public void deleteDocument(@NonNull @NotNull DocumentSnapshot documentSnapshot,
+                               @Nullable DataCallbacks.InsertUpdateDeleteCallback<DocumentSnapshot> callback){
+
+        documentSnapshot.getReference()
+                .delete()
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull @NotNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            if(callback != null){
+                                callback.onSuccess(null);
+                            }
+                            return;
+                        }
+
+                        // here delete operation failed
+                        Timber.w(task.getException());
+                        if(callback != null){
+                            callback.onFailure(documentSnapshot);
+                        }
+                    }
+                });
+    }
+
+}
+
+
