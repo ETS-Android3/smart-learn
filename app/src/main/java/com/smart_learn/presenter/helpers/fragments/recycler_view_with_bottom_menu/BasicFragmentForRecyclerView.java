26ce16ec78ad8fab953483479582704aaabc678a
Marius-RO <blw_matt@rocketmail.com>
2021-07-13 10:49:11 +0300

    Update basic recycler view fragment
    * Update BasicFragmentForRecyclerView by adding support for action mode and
      specific bottom sheet when action mode is started.

--- app/src/main/java/com/smart_learn/presenter/helpers/fragments/recycler_view_with_bottom_menu/BasicFragmentForRecyclerView.java
+++ app/src/main/java/com/smart_learn/presenter/helpers/fragments/recycler_view_with_bottom_menu/BasicFragmentForRecyclerView.java
@@ -2,25 +2,35 @@ package com.smart_learn.presenter.helpers.fragments.recycler_view_with_bottom_me
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewStub;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.ActionMode;
+import androidx.appcompat.widget.LinearLayoutCompat;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.smart_learn.R;
 import com.smart_learn.databinding.FragmentBasicForRecyclerViewBinding;
+import com.smart_learn.presenter.helpers.Callbacks;
 import com.smart_learn.presenter.helpers.ItemDecoration;
 import com.smart_learn.presenter.helpers.PresenterHelpers;
 import com.smart_learn.presenter.helpers.Utilities;
 import com.smart_learn.presenter.helpers.fragments.helpers.BasicFragment;
 
 import org.jetbrains.annotations.NotNull;
+
+import lombok.Getter;
 
 /**
  * The main fragment from which all the fragments of the application which contains a standard
@@ -49,12 +59,55 @@ import org.jetbrains.annotations.NotNull;
 public abstract class BasicFragmentForRecyclerView<VM extends BasicViewModelForRecyclerView<?>> extends BasicFragment<VM>
         implements PresenterHelpers.FragmentRecyclerViewHelper {
 
+    private static final int NO_RESOURCE_SELECTED = -1;
+
     protected FragmentBasicForRecyclerViewBinding binding;
 
+    protected CoordinatorLayout includeRVLayout;
     protected SwipeRefreshLayout swipeRefreshLayout;
     protected RecyclerView recyclerView;
     protected FloatingActionButton floatingActionButton;
     protected TextView emptyLabel;
+
+    @Getter
+    protected ActionMode actionMode;
+
+    // These values will be used when action mode is active if a specific bottom sheet was specified.
+    protected LinearLayoutCompat bottomSheetLayout;
+    protected BottomSheetBehavior<LinearLayoutCompat> bottomSheetBehavior;
+
+
+    /**
+     * Override if you want to use a bottom sheet when action mode is active.
+     *
+     * If you set this to true, methods 'getBottomSheetLayout()' and 'getParentBottomSheetLayoutId()'
+     * must be overridden also in order to specify the bottom sheet.
+     *
+     * @return true to use, false otherwise (default behaviour).
+     * */
+    protected boolean isBottomSheetUsed() {
+        return false;
+    }
+
+
+    /**
+     * Override if you want to use a specific bottom sheet when action mode is active.
+     *
+     * @return Resource layout of the bottom sheet.
+     * */
+    protected int getBottomSheetLayout() {
+        return NO_RESOURCE_SELECTED;
+    }
+
+
+    /**
+     * Override if you want to use a specific bottom sheet when action mode is active.
+     *
+     * @return Resource id of the parent bottom sheet layout.
+     * */
+    protected int getParentBottomSheetLayoutId() {
+        return NO_RESOURCE_SELECTED;
+    }
 
 
     /**
@@ -124,6 +177,7 @@ public abstract class BasicFragmentForRecyclerView<VM extends BasicViewModelForR
 
     protected void setLayoutUtilities(){
         // set views
+        includeRVLayout = binding.includeLayoutRecyclerViewFragmentBasicForRecyclerView.parentLayoutIncludeLayoutRecyclerViewWithBottomNav;
         swipeRefreshLayout = binding.includeLayoutRecyclerViewFragmentBasicForRecyclerView.swipeRefreshLayoutRecyclerViewWithBottomNav;
         recyclerView = binding.includeLayoutRecyclerViewFragmentBasicForRecyclerView.rvIncludeLayoutRecyclerViewWithBottomNav;
         floatingActionButton = binding.includeLayoutRecyclerViewFragmentBasicForRecyclerView.floatingBtnAddIncludeLayoutRecyclerViewWithBottomNav;
@@ -136,6 +190,19 @@ public abstract class BasicFragmentForRecyclerView<VM extends BasicViewModelForR
 
         // fragments can choose specific description when are no items in the recycler view
         emptyLabel.setText(getEmptyLabelDescriptionResourceId());
+
+        // set values for bottom sheet is this is set
+        if(isBottomSheetUsed()){
+            if(getBottomSheetLayout() == NO_RESOURCE_SELECTED || getParentBottomSheetLayoutId() == NO_RESOURCE_SELECTED){
+                throw new UnsupportedOperationException("You must give a specific layout, if you want to" +
+                        " use the bottom sheet");
+            }
+            ViewStub viewStub = (ViewStub) requireActivity().findViewById(R.id.view_stub_fragment_basic_for_recycler_view);
+            viewStub.setLayoutResource(getBottomSheetLayout());
+            viewStub.inflate();
+            bottomSheetLayout = (LinearLayoutCompat) requireActivity().findViewById(getParentBottomSheetLayoutId());
+            bottomSheetBehavior = Utilities.Activities.setPersistentBottomSheet(bottomSheetLayout);
+        }
 
         setSwipeRefreshLayout();
 
@@ -187,5 +254,45 @@ public abstract class BasicFragmentForRecyclerView<VM extends BasicViewModelForR
     public void scrollToPosition(int position) {
         // https://stackoverflow.com/questions/30805262/how-to-get-recyclerview-to-view-a-certain-position-without-scrolling/30805540#30805540
         recyclerView.scrollToPosition(position);
+    }
+
+
+    protected void startActionMode(@NonNull Callbacks.ActionModeCustomCallback actionModeCustomCallback){
+        actionMode = ((AppCompatActivity)requireActivity()).startSupportActionMode(new ActionMode.Callback() {
+            @Override
+            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                if(!showFloatingActionButton()){
+                    Utilities.Activities.showPersistentBottomSheet(null, includeRVLayout, bottomSheetLayout, bottomSheetBehavior);
+                }
+                else{
+                    Utilities.Activities.showPersistentBottomSheet(floatingActionButton, includeRVLayout, bottomSheetLayout, bottomSheetBehavior);
+                }
+                actionModeCustomCallback.onCreateActionMode();
+                return true;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                return false;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode mode) {
+                mode.finish();
+                actionMode = null;
+                if(!showFloatingActionButton()){
+                    Utilities.Activities.hidePersistentBottomSheet(null, includeRVLayout, bottomSheetBehavior);
+                }
+                else{
+                    Utilities.Activities.hidePersistentBottomSheet(floatingActionButton, includeRVLayout, bottomSheetBehavior);
+                }
+                actionModeCustomCallback.onDestroyActionMode();
+            }
+        });
     }
 }
