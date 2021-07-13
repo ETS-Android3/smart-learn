928f162205669ca329351d51959f49bfd4cb08d2
Marius-RO <blw_matt@rocketmail.com>
2021-07-13 12:58:35 +0300

    Rename nav_graph_activity_notebook
    * Rename into 'nav_graph_activity_guest_notebook' because it will be
      the associated navigation graph for GuestNotebookActivity.

--- app/src/main/java/com/smart_learn/presenter/activities/notebook/guest/fragments/GuestNotebookActivity.java
+++ app/src/main/java/com/smart_learn/presenter/activities/notebook/guest/fragments/GuestNotebookActivity.java
@@ -22,7 +22,7 @@ public class GuestNotebookActivity extends NotebookActivity<GuestNotebookSharedV
 
     @Override
     protected int getNavigationGraphResource() {
-        return R.navigation.nav_graph_activity_notebook;
+        return R.navigation.nav_graph_activity_guest_notebook;
     }
 
     @Override
@@ -37,30 +37,31 @@ public class GuestNotebookActivity extends NotebookActivity<GuestNotebookSharedV
                     public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
                         switch (item.getItemId()){
                             // fragment with bottom navigation view VISIBLE
-                            case R.id.guest_home_lesson_fragment_nav_graph_activity_notebook:
-                                navController.navigate(R.id.guest_home_lesson_fragment_nav_graph_activity_notebook,null,
-                                        Utilities.Activities.getVisibleBottomMenuNavOptions(R.id.guest_lessons_fragment_nav_graph_activity_notebook));
+                            case R.id.guest_home_lesson_fragment_nav_graph_activity_guest_notebook:
+                                navController.navigate(R.id.guest_home_lesson_fragment_nav_graph_activity_guest_notebook,null,
+                                        Utilities.Activities.getVisibleBottomMenuNavOptions(R.id.guest_lessons_fragment_nav_graph_activity_guest_notebook));
                                 return true;
                             // fragment with bottom navigation view VISIBLE
-                            case R.id.guest_words_fragment_nav_graph_activity_notebook:
-                                navController.navigate(R.id.guest_words_fragment_nav_graph_activity_notebook,null,
-                                        Utilities.Activities.getVisibleBottomMenuNavOptions(R.id.guest_lessons_fragment_nav_graph_activity_notebook));
+                            case R.id.guest_words_fragment_nav_graph_activity_guest_notebook:
+                                navController.navigate(R.id.guest_words_fragment_nav_graph_activity_guest_notebook,null,
+                                        Utilities.Activities.getVisibleBottomMenuNavOptions(R.id.guest_lessons_fragment_nav_graph_activity_guest_notebook));
                                 return true;
                         }
                         return false;
                     }
                 }, null);
+
     }
 
     /** Fragment has bottom navigation view HIDDEN. */
     public void goToHomeLessonFragment(){
-        navController.navigate(R.id.action_guest_lessons_fragment_to_guest_home_lesson_fragment_nav_graph_activity_notebook,null,
-                Utilities.Activities.getEnterBottomMenuNavOptions(R.id.guest_home_lesson_fragment_nav_graph_activity_notebook));
+        navController.navigate(R.id.action_guest_lessons_fragment_to_guest_home_lesson_fragment_nav_graph_activity_guest_notebook,null,
+                Utilities.Activities.getEnterBottomMenuNavOptions(R.id.guest_home_lesson_fragment_nav_graph_activity_guest_notebook));
     }
 
     /** Fragment has bottom navigation view HIDDEN. */
     public void goToHomeWordFragment(){
-        navController.navigate(R.id.action_guest_words_fragment_nav_graph_activity_notebook_to_guest_home_word_fragment_nav_graph_activity_notebook,null,
-                Utilities.Activities.getExitBottomMenuNavOptions(R.id.guest_home_word_fragment_nav_graph_activity_notebook));
+        navController.navigate(R.id.action_guest_words_fragment_nav_graph_activity_notebook_to_guest_home_word_fragment_nav_graph_activity_guest_notebook,null,
+                Utilities.Activities.getExitBottomMenuNavOptions(R.id.guest_home_word_fragment_nav_graph_activity_guest_notebook));
     }
 }
