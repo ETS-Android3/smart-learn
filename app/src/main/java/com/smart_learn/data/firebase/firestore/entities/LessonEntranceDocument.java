28f293c1891555a31f51f55f897dd5c4ace0ab67
Marius-RO <blw_matt@rocketmail.com>
2021-07-20 12:48:22 +0300

    Update WordDocument
    * Main updates:
      - prepare WordDocument entity for Firestore
      - add LessonEntranceDocument for keeping common info`s for words and expressions

---
+++ app/src/main/java/com/smart_learn/data/firebase/firestore/entities/LessonEntranceDocument.java
@@ -0,0 +1,57 @@
+package com.smart_learn.data.firebase.firestore.entities;
+
+import com.smart_learn.data.firebase.firestore.entities.helpers.BasicNotebookCommonDocument;
+import com.smart_learn.data.firebase.firestore.entities.helpers.DocumentMetadata;
+
+import java.util.HashMap;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public abstract class LessonEntranceDocument extends BasicNotebookCommonDocument {
+
+    public interface Fields {
+        String IS_FAVOURITE_FIELD_NAME = "isFavourite";
+        String LANGUAGE_FIELD_NAME = "language";
+        String TRANSLATIONS_FIELD_NAME = "translations";
+    }
+
+    private boolean isFavourite;
+    private String language;
+    // array list of translations transformed in string with Gson
+    protected String translations;
+
+    public LessonEntranceDocument() {
+        // needed for Firestore
+    }
+
+    public LessonEntranceDocument(DocumentMetadata documentMetadata, String notes, boolean isFavourite, String language, String translations) {
+        super(documentMetadata, notes);
+        this.isFavourite = isFavourite;
+        this.language = language;
+        this.translations = translations;
+    }
+
+    public static HashMap<String, Object> convertDocumentToHashMap(LessonEntranceDocument document){
+        if(document == null){
+            return new HashMap<>();
+        }
+
+        HashMap<String, Object> data = BasicNotebookCommonDocument.convertDocumentToHashMap(document);
+        data.put(Fields.IS_FAVOURITE_FIELD_NAME, document.getFavourite());
+        data.put(Fields.LANGUAGE_FIELD_NAME, document.getLanguage());
+        data.put(Fields.TRANSLATIONS_FIELD_NAME, document.getTranslations());
+
+        return data;
+    }
+
+    /**
+     * This getter must have this form in order to work with Firestore. If you leave getter generated
+     * by Lombok using @Getter then isFavourite() will be generated and will not work with Firestore.
+     * */
+    public boolean getFavourite() {
+        return isFavourite;
+    }
+}
