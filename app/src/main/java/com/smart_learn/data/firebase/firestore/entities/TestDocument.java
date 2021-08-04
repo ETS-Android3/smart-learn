4b90e71b4579286e2db8b900c777fc3ad86f9d75
Marius-RO <blw_matt@rocketmail.com>
2021-08-04 18:15:09 +0300

    Add new fields related to the user's online tests in TestDocument

--- app/src/main/java/com/smart_learn/data/firebase/firestore/entities/TestDocument.java
+++ app/src/main/java/com/smart_learn/data/firebase/firestore/entities/TestDocument.java
@@ -1,8 +1,13 @@
 package com.smart_learn.data.firebase.firestore.entities;
 
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.Exclude;
 import com.smart_learn.data.entities.Test;
 import com.smart_learn.data.firebase.firestore.entities.helpers.DocumentMetadata;
 
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import lombok.Getter;
@@ -19,6 +24,11 @@ public class TestDocument extends Test {
     public interface Fields {
         String IS_COUNTED_AS_FINISHED_FIELD_NAME = "countedAsFinished";
         String IS_ONLINE_FIELD_NAME = "online";
+        String CONTAINER_TEST_ID_FIELD_NAME = "containerTestId";
+        String USER_DISPLAY_NAME_FIELD_NAME = "userDisplayName";
+        String USER_EMAIL_FIELD_NAME = "userEmail";
+        String USER_PROFILE_PHOTO_URL_FIELD_NAME = "userProfilePhotoUrl";
+        String PARTICIPANTS_FIELD_NAME = "participants";
     }
 
     @Getter
@@ -28,6 +38,19 @@ public class TestDocument extends Test {
     private boolean isCountedAsFinished;
     // mark that test is online or local
     private boolean isOnline;
+    // used to store id of the container test for a fast access if needed
+    private String containerTestId = "";
+    // used to store UID of the participants
+    private ArrayList<String> participants = new ArrayList<>();
+
+    // used for a faster access at user data
+    private String userDisplayName = "";
+    private String userEmail = "";
+    private String userProfilePhotoUrl = "";
+
+    @Exclude
+    @Nullable
+    private ArrayList<DocumentSnapshot> selectedFriends;
 
     public TestDocument() {
         // needed for Firestore
@@ -36,6 +59,26 @@ public class TestDocument extends Test {
     public TestDocument(DocumentMetadata documentMetadata) {
         super();
         this.documentMetadata = documentMetadata;
+    }
+
+    public void setContainerTestId(String containerTestId) {
+        this.containerTestId = containerTestId == null ? "" : containerTestId;
+    }
+
+    public void setUserDisplayName(String userDisplayName) {
+        this.userDisplayName = userDisplayName == null ? "" : userDisplayName;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail == null ? "" : userEmail;
+    }
+
+    public void setUserProfilePhotoUrl(String userProfilePhotoUrl) {
+        this.userProfilePhotoUrl = userProfilePhotoUrl == null ? "" : userProfilePhotoUrl;
+    }
+
+    public void setParticipants(ArrayList<String> participants) {
+        this.participants = participants == null ? new ArrayList<>() : participants;
     }
 
     public static HashMap<String, Object> convertDocumentToHashMap(TestDocument testDocument){
@@ -47,8 +90,23 @@ public class TestDocument extends Test {
         data.put(DocumentMetadata.Fields.DOCUMENT_METADATA_FIELD_NAME, DocumentMetadata.convertDocumentToHashMap(testDocument.getDocumentMetadata()));
         data.put(Fields.IS_COUNTED_AS_FINISHED_FIELD_NAME, testDocument.isCountedAsFinished());
         data.put(Fields.IS_ONLINE_FIELD_NAME, testDocument.isOnline());
+        data.put(Fields.CONTAINER_TEST_ID_FIELD_NAME, testDocument.getContainerTestId());
+        data.put(Fields.USER_DISPLAY_NAME_FIELD_NAME, testDocument.getUserDisplayName());
+        data.put(Fields.USER_EMAIL_FIELD_NAME, testDocument.getUserEmail());
+        data.put(Fields.USER_PROFILE_PHOTO_URL_FIELD_NAME, testDocument.getUserProfilePhotoUrl());
+        data.put(Fields.PARTICIPANTS_FIELD_NAME, testDocument.getParticipants());
 
         return data;
+    }
+
+    /**
+     * Field selected is NOT needed in the firestore document.
+     *
+     * https://stackoverflow.com/questions/49865558/firestore-where-exactly-to-put-the-exclude-annotation
+     * */
+    @Exclude
+    public @Nullable ArrayList<DocumentSnapshot> getSelectedFriends() {
+        return selectedFriends;
     }
 
 }
